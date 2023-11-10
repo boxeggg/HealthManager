da24ff3c08104153e2fe67fa945e39d583377cb0
boxeggg <kamilszydlowski53@Gmail.com>
2023-11-10 15:42:28 +0100

    Dodaj pliki projektów.

---
+++ MenedżerBadań/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MenedżerBadań.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
