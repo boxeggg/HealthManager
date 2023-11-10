aac3290237c229c0e23478d06f41de4e7e20db9a
boxeggg <kamilszydlowski53@Gmail.com>
2023-11-10 17:47:47 +0100

    added viewmodel "mojeBadaniaViewData" which simulates data from database

--- MenedżerBadań/Controllers/MojeBadaniaController.cs
+++ MenedżerBadań/Controllers/MojeBadaniaController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MenedżerBadań.Models;
+using MenedżerBadań.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MenedżerBadań.Controllers
 {
     public class MojeBadania : Controller
     {
+        private List<Badania> listaBadan = new List<Badania>()
+        {
+            new Badania{ Id = 0, Name = "Poziom glukozy we krwi"},
+            new Badania{ Id = 1, Name = "Ciśnienie krwi"},
+            new Badania{ Id = 3, Name = "Morforlogia krwi"},
+            new Badania{ Id = 4, Name = "Koagulogram"},
+        };
         public IActionResult Index()
         {
-            return View();
+            MojeBadaniaViewData vm = new MojeBadaniaViewData();
+            vm.Badania = listaBadan;
+            return View(vm);
         }
     }
 }
---
+++ MenedżerBadań/ViewModels/MojeBadaniaViewData.cs
@@ -0,0 +1,10 @@
+﻿using MenedżerBadań.Models;
+namespace MenedżerBadań.ViewModels
+  
+{
+    public class MojeBadaniaViewData
+    {
+    public List<Badania> Badania { get; set; }
+
+    }
+}
