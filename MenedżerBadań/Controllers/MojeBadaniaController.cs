c63da1cd0633b63c1b7c9e3888e0ab529a7395e6
boxeggg <kamilszydlowski53@Gmail.com>
2023-11-10 16:31:15 +0100

    dodano podstrone "moje badania"

---
+++ MenedżerBadań/Controllers/MojeBadaniaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MenedżerBadań.Controllers
+{
+    public class MojeBadania : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
