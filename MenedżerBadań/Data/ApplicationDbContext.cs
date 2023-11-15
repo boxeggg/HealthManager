8733cb29b0fd4d6abe46cd1b45e0129a22f86a59
boxeggg <kamilszydlowski53@Gmail.com>
2023-11-15 11:07:01 +0100

    updated appdbcontext

--- MenedżerBadań/Data/ApplicationDbContext.cs
+++ MenedżerBadań/Data/ApplicationDbContext.cs
@@ -7,11 +7,11 @@ namespace MenedżerBadań.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<BodyEntity> BodyEntity { get; set; }
-        public DbSet<DeviceEntity> deviceEntity { get; set; }
+        public DbSet<DeviceEntity> DeviceEntity { get; set; }
         public DbSet<MeasurementEntity> MeasurementEntity { get; set; }
         public DbSet<MeasurementTypeEntity> MeasurementTypeEntity { get; set; }
         public DbSet<ProfileEntity> ProfileEntity { get; set; }
-        public DbSet<UserEntity> UserEntity { get; set; }
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ MenedżerBadań/Data/Migrations/20231115100445_initial.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MenedżerBadań.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserEntity");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_deviceEntity",
+                table: "deviceEntity");
+
+            migrationBuilder.RenameTable(
+                name: "deviceEntity",
+                newName: "DeviceEntity");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DeviceEntity",
+                table: "DeviceEntity",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DeviceEntity",
+                table: "DeviceEntity");
+
+            migrationBuilder.RenameTable(
+                name: "DeviceEntity",
+                newName: "deviceEntity");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_deviceEntity",
+                table: "deviceEntity",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "UserEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEntity", x => x.Id);
+                });
+        }
+    }
+}
