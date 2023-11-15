1539f1c10a1053988d6198fbfb5728080e44fb80
boxeggg <kamilszydlowski53@Gmail.com>
2023-11-15 11:33:31 +0100

    Created initial repository

---
+++ MenedżerBadań/Repositories/MeasurementRepository.cs
@@ -0,0 +1,49 @@
+﻿using HealthManager.Data.Entities;
+using System.Diagnostics.Metrics;
+
+using MenedżerBadań.Data;
+
+namespace HealthManager.Repositories
+{
+    public interface IMeasurementRepository
+    {
+        MeasurementEntity GetMeasurementById(int id);
+        MeasurementEntity DeleteMeasurement(int id);
+        MeasurementEntity InsertMeasurement(MeasurementEntity measurementEntity);
+        List<MeasurementEntity> GetAll();
+    }
+    public class MeasurementRepository : IMeasurementRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public MeasurementRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public MeasurementEntity DeleteMeasurement(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MeasurementEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MeasurementEntity GetMeasurementById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MeasurementEntity GetTaskById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MeasurementEntity InsertMeasurement(MeasurementEntity measurementEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
